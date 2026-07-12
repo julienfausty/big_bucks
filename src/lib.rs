7dd4c585cbf5347955b911c20fb9b0d06d1ccece
jfausty <julienfausty@gmail.com>
2026-07-12 14:39:01 +0200

    build: refactor exploration binary and crate into library

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod fetch;
+pub use fetch::{MarketChartQuery, query_market_chart};
+
+pub mod interp;
+pub use interp::rebase;
+
+pub mod regression;
+pub use regression::{check_augmented_dicky_fuller, check_johansen};
