d15694a2ce6d7fcb04dbfc7d02c32b6c42576263
jfausty <julienfausty@gmail.com>
2026-07-14 17:53:12 +0200

    feat: implement trading loop in trade binary

--- src/trade.rs
+++ src/trade.rs
@@ -10,13 +10,17 @@ use tokio::time::{Duration, sleep};
 
 use std::collections::HashMap;
 
-use big_bucks::fetch::{MarketChartQuery, query_market_chart};
+use big_bucks::fetch::{MarketChartQuery, PricePairPipe, query_market_chart};
 use big_bucks::interp::{RebasedSeries, rebase};
 use big_bucks::orders::{Confirmation, Order};
-use big_bucks::signals::{Signal, StatArbSignal};
+use big_bucks::signals::Signal;
 use big_bucks::strategy::{StatArbModel, StatArbPolicy};
 
 const ADF_THRESHOLD: f64 = -2.0;
+const ENTRY_SCORE: f64 = 1.25;
+const EXIT_SCORE: f64 = 0.25;
+const CUT_LOSS: f64 = 3.0;
+const MAX_RISK: f64 = 0.05;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Account {}
@@ -120,12 +124,86 @@ async fn main() -> Result<(), String> {
     log::info!("Trading pair {trading_pair:?}");
 
     let mut model_pipe = ModelPipe::new(trading_pair.clone()).await?;
-    let model = model_pipe.newest().await?;
+    let mut price_pipe = PricePairPipe::new(trading_pair.clone()).await?;
 
-    log::info!(
-        "Model computed {}",
-        serde_json::to_string(&model).unwrap_or_else(|e| format!("{:?}", e))
-    );
+    loop {
+        let (timestamp, p0, p1) = price_pipe.newest_change().await?;
+        log::info!(
+            "New price action (t, {}, {}): ({}, {}, {})",
+            trading_pair.0,
+            trading_pair.1,
+            timestamp,
+            p0,
+            p1
+        );
 
-    Ok(())
+        let account_state = account.state().await?;
+        log::info!(
+            "Account state: Cash {}, Portfolio {:?}",
+            account_state.0,
+            account_state.1
+        );
+
+        let policy = StatArbPolicy::new(
+            account_state.0,
+            account_state.1,
+            ENTRY_SCORE,
+            EXIT_SCORE,
+            CUT_LOSS,
+            MAX_RISK,
+        );
+        log::info!(
+            "Policy initialized {}",
+            serde_json::to_string(&policy).unwrap_or_else(|e| format!("{:?}", e))
+        );
+
+        let signal = match model_pipe.newest().await {
+            Ok(model) => {
+                log::info!(
+                    "Model computed {}",
+                    serde_json::to_string(&model).unwrap_or_else(|e| format!("{:?}", e))
+                );
+                Signal::StatArb(model.signal(timestamp, (p0, p1)))
+            }
+            Err(_) => Signal::MarketUncertain,
+        };
+
+        log::info!(
+            "Signal {}",
+            serde_json::to_string(&signal).unwrap_or_else(|e| format!("{:?}", e))
+        );
+
+        let orders = policy.evaluate(signal);
+
+        log::info!(
+            "Orders generated {}",
+            serde_json::to_string(&orders).unwrap_or_else(|e| format!("{:?}", e))
+        );
+
+        let mut order_fail = false;
+        match orders {
+            Some(orders) => {
+                for order in orders.into_iter() {
+                    match account.execute(order).await {
+                        Ok(confirmation) => log::info!(
+                            "Order completed {}",
+                            serde_json::to_string(&confirmation)
+                                .unwrap_or_else(|e| format!("{:?}", e))
+                        ),
+                        Err(message) => {
+                            log::error!("Could not complete order: {}", message);
+                            order_fail = true;
+                        }
+                    };
+                }
+            }
+            None => (),
+        };
+
+        if order_fail {
+            break;
+        }
+    }
+
+    Err("Program exited loop".to_string())
 }
